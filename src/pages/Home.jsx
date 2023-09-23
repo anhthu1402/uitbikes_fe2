1b4294026020d782f61dff59c7066ce33846b89e
anhthu1402 <20520792@gm.uit.edu,vn>
2023-09-23 23:58:54 +0700

    add logo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Logo from "../assets/images/uit_bikes_logo.svg";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <img src={Logo} alt="" />
+    </div>
+  );
 }
 
 export default Home;
